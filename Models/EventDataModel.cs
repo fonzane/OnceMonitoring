ab2400236c9bec5cccce201b9c7452ad49262c82
fonzane <timschaffert@yahoo.de>
2025-01-22 15:47:27 +0100

    completed the EventDataModel to include more information from the event data stream

--- Models/EventDataModel.cs
+++ Models/EventDataModel.cs
@@ -2,15 +2,15 @@ namespace OnceMonitoring.Models
 {
     public class EventDataModel
     {
-        public EventSource event_source { get; set; }
-        public Organisation organisation {get;set;}
-        public EventSeverity event_severity {get; set;}
+        public EventSource? event_source { get; set; }
+        public Organisation? organisation { get; set; }
+        public EventSeverity? event_severity { get; set; }
         public SIM? sim { get; set; }
         public string? description { get; set; }
         public bool alert { get; set; }
         public long id { get; set; }
         public Endpoint? endpoint { get; set; }
-        public EventType event_type { get; set; }
+        public EventType? event_type { get; set; }
         public DateTime timestamp { get; set; }
     }
 
@@ -40,7 +40,9 @@ namespace OnceMonitoring.Models
 
     public class IMSI
     {
-
+        public int id { get; set; }
+        public string? imsi { get; set; }
+        public DateTime import_date { get; set; }
     }
 
     public class SIM
@@ -58,5 +60,55 @@ namespace OnceMonitoring.Models
         public string? ip_address { get; set; }
         public string? tags { get; set; }
         public string? imei { get; set; }
+    }
+
+    public class Details
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public Country? country { get; set; }
+        public PdpContext? pdp_context { get; set; }
+        public Volume? volume { get; set; }
+
+    }
+
+    public class Country
+    {
+        public int id { get; set; }
+        public string? name { get; set; }
+        public string? country_code { get; set; }
+        public string? mcc { get; set; }
+        public string? iso_code { get; set; }
+    }
+
+    public class PdpContext
+    {
+        public int pdp_context_id { get; set; }
+        public DateTime tunnel_created { get; set; }
+        public string? gpt_version { get; set; }
+        public string? ggsn_control_plane_ip_address { get; set; }
+        public string? ggsn_data_plane_ip_address { get; set; }
+        public string? sgsn_control_plane_ip_address { get; set; }
+        public string? sgsn_data_plane_ip_address { get; set; }
+        public string? region { get; set; }
+        public string? breakout_ip { get; set; }
+        public string? apn { get; set; }
+        public int nsapi { get; set; }
+        public string? ue_ip_address { get; set; }
+        public string? imeisv { get; set; }
+        public string? mcc { get; set; }
+        public string? mnc { get; set; }
+        public int lac { get; set; }
+        public int sac { get; set; }
+        public int rac { get; set; }
+        public int ci { get; set; }
+        public int rat_type { get; set; }
+    }
+
+    public class Volume
+    {
+        public decimal rx { get; set; }
+        public decimal tx { get; set; }
+        public decimal total { get; set; }
     }
 }
